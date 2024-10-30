7950fa38ecd5f4816ed97a0d87fbae4fbbc9158a
Prashant Kokande <99704639+Prashantkokande@users.noreply.github.com>
2024-10-30 23:17:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict():
     return render_template('index.html', prediction_text=f'The predicted species is {result}')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0", port=5000)
